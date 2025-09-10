fb3d24cd4f10b5d91e73de3a4b875dcff1a89403
ttinonin <tinonin935@gmail.com>
2025-09-10 13:27:07 -0300

    Basic error handling

--- OpenGl/src/Application.cpp
+++ OpenGl/src/Application.cpp
@@ -5,6 +5,24 @@
 #include <fstream>
 #include <sstream>
 
+#define ASSERT(x) if (!(x)) __debugbreak();
+#define GlCall(x) GlClearError();\
+    x;\
+    ASSERT(GlLogCall(#x, __FILE__, __LINE__))
+
+static void GlClearError() {
+    while (glGetError() != GL_NO_ERROR);
+}
+
+static bool GlLogCall(const char* function, const char* file, int line) {
+    while (GLenum error = glGetError()) {
+        std::cout << "[OpenGl Error] (" << error << "): " << function <<  " " << file << ": " << line << std::endl;
+        return false;
+    }
+
+    return true;
+}
+
 struct ShaderProgramSource {
     std::string VertexSource;
     std::string FragmentSource;
@@ -139,7 +157,7 @@ int main(void)
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
+        GlCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
